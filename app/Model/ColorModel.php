bbf8dca1d8da7ad1e07101411052191428f652a2
Kolja <55852574+KoljaL@users.noreply.github.com>
2021-08-20 12:58:47 +0200

    Update ColorModel.php

--- app/Model/ColorModel.php
+++ app/Model/ColorModel.php
@@ -21,49 +21,49 @@ class ColorModel extends Base
     protected $default_colors = array(
         'yellow' => array(
             'name' => 'Yellow',
-            'background' => 'rgb(245, 247, 196)',
-            'border' => 'rgb(223, 227, 45)',
+            'background' => 'rgb(230, 192, 123)',
+            'border' => 'rgb(161, 134, 86)',
         ),
         'blue' => array(
             'name' => 'Blue',
-            'background' => 'rgb(219, 235, 255)',
-            'border' => 'rgb(168, 207, 255)',
+            'background' => 'rgb(97, 174, 238)',
+            'border' => 'rgb(67, 121, 166)',
         ),
         'green' => array(
             'name' => 'Green',
-            'background' => 'rgb(189, 244, 203)',
-            'border' => 'rgb(74, 227, 113)',
+            'background' => 'rgb(152, 195, 121)',
+            'border' => 'rgb(106, 136, 84)',
         ),
         'purple' => array(
             'name' => 'Purple',
-            'background' => 'rgb(223, 176, 255)',
-            'border' => 'rgb(205, 133, 254)',
+            'background' => 'rgb(198, 120, 221)',
+            'border' => 'rgb(138, 84, 154)',
         ),
         'red' => array(
             'name' => 'Red',
-            'background' => 'rgb(255, 187, 187)',
-            'border' => 'rgb(255, 151, 151)',
+            'background' => 'rgb(190, 80, 70)',
+            'border' => 'rgb(133, 56, 49)',
         ),
         'orange' => array(
             'name' => 'Orange',
-            'background' => 'rgb(255, 215, 179)',
-            'border' => 'rgb(255, 172, 98)',
+            'background' => 'rgb(209, 154, 102)',
+            'border' => 'rgb(146, 107, 71)',
         ),
         'grey' => array(
             'name' => 'Grey',
             'background' => 'rgb(238, 238, 238)',
             'border' => 'rgb(204, 204, 204)',
         ),
-        'brown' => array(
-            'name' => 'Brown',
-            'background' => '#d7ccc8',
-            'border' => '#4e342e',
-        ),
-        'deep_orange' => array(
-            'name' => 'Deep Orange',
-            'background' => '#ffab91',
-            'border' => '#e64a19',
-        ),
+        // 'brown' => array(
+        //     'name' => 'Brown',
+        //     'background' => '#d7ccc8',
+        //     'border' => '#4e342e',
+        // ),
+        // 'deep_orange' => array(
+        //     'name' => 'Deep Orange',
+        //     'background' => '#ffab91',
+        //     'border' => '#e64a19',
+        // ),
         'dark_grey' => array(
             'name' => 'Dark Grey',
             'background' => '#cfd8dc',
@@ -71,34 +71,34 @@ class ColorModel extends Base
         ),
         'pink' => array(
             'name' => 'Pink',
-            'background' => '#f48fb1',
-            'border' => '#d81b60',
+            'background' => '#e06c75',
+            'border' => '#9c4b51',
         ),
         'teal' => array(
             'name' => 'Teal',
-            'background' => '#80cbc4',
-            'border' => '#00695c',
+            'background' => '#56b6c2',
+            'border' => '#3c7f87',
         ),
-        'cyan' => array(
-            'name' => 'Cyan',
-            'background' => '#b2ebf2',
-            'border' => '#00bcd4',
-        ),
-        'lime' => array(
-            'name' => 'Lime',
-            'background' => '#e6ee9c',
-            'border' => '#afb42b',
-        ),
-        'light_green' => array(
-            'name' => 'Light Green',
-            'background' => '#dcedc8',
-            'border' => '#689f38',
-        ),
-        'amber' => array(
-            'name' => 'Amber',
-            'background' => '#ffe082',
-            'border' => '#ffa000',
-        ),
+        // 'cyan' => array(
+        //     'name' => 'Cyan',
+        //     'background' => '#b2ebf2',
+        //     'border' => '#00bcd4',
+        // ),
+        // 'lime' => array(
+        //     'name' => 'Lime',
+        //     'background' => '#e6ee9c',
+        //     'border' => '#afb42b',
+        // ),
+        // 'light_green' => array(
+        //     'name' => 'Light Green',
+        //     'background' => '#dcedc8',
+        //     'border' => '#689f38',
+        // ),
+        // 'amber' => array(
+        //     'name' => 'Amber',
+        //     'background' => '#ffe082',
+        //     'border' => '#ffa000',
+        // ),
     );
 
     /**
